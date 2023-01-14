0763aa888a4a6f5e42fb6e7e0741c9e1696d4766
Daniel Boakye <danielboakye98@yahoo.com>
2023-01-14 02:35:23 +0000

    add unit tests

---
+++ test/BasicMarketplace.js
@@ -0,0 +1,27 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { describe } = require("node:test");
+
+describe("BasicMarketplace", function(){
+    it("Should return a new product once deployed", async function(){
+
+        const Contract = await ethers.getContractFactory("BasicMarketplace");
+        const contract = await Contract.deploy();
+        await contract.deployed();
+
+        expect(await contract.numProduct()).to.equal(1);
+    });
+
+    it("Should create a new product", async function(){
+
+        const Contract = await ethers.getContractFactory("BasicMarketplace");
+        const contract = await Contract.deploy();
+        await contract.deployed();
+
+        const addProductTx = await contract.addProduct("test product", 100);
+
+        await addProductTx.wait();
+
+        expect(await contract.numProduct()).to.equal(2);
+    });
+});
